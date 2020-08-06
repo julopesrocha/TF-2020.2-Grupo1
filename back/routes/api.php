f22d37ebe773d5e719dae6865948230d5f9b74eb
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-08-06 17:13:44 -0300

    +routes do passport

--- back/routes/api.php
+++ back/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Passport
+Route::POST('register', 'API\PassportController@register');
+route::POST('login', 'API\PassportController@login');
+
+Route::group(['middleware' =>'auth:api'], function(){
+    Route::GET('logout', 'API\PassportController@logout');
+    Route::POST('getDetails', 'API\PassportController@getDetails');
+});
