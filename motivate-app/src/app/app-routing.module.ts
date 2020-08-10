42a504a9284f014d2590de003bd0fbf7a6fe2e53
LucianaVit <luciana.vbnd@gmail.com>
2020-08-10 13:07:21 -0300

    +componentizaçao+tabbar+toolbar*botão

--- motivate-app/src/app/app-routing.module.ts
+++ motivate-app/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'home-deslog',
+    loadChildren: () => import('./pages/home-deslog/home-deslog.module').then( m => m.HomeDeslogPageModule)
   }
+
 ];
 @NgModule({
   imports: [
