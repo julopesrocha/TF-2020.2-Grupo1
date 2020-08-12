81a08649c24a8f13e844e7c3c780e75d58a9ae70
gabimattos <50138800+gabimattos@users.noreply.github.com>
2020-08-12 20:31:49 -0300

    + cria page perfil

--- motivate-app/src/app/app-routing.module.ts
+++ motivate-app/src/app/app-routing.module.ts
@@ -17,7 +17,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   }
+
 
 ];
 @NgModule({
