417015f7abc08bd499b370c865e5af211b54dd4a
gabimattos <50138800+gabimattos@users.noreply.github.com>
2020-08-13 14:19:40 -0300

    # altera app.module.ts

---
+++ motivate-app/android/app/src/main/assets/public/app/tab1/tab1.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(public authservice: AuthService, private router: Router) {}
+
+  logout() {
+    this.authservice.logout().subscribe(
+        (res) => {
+            console.log(res);
+            localStorage.removeItem('userToken');
+            localStorage.removeItem('Usuario');
+            
+            this.router.navigate(['/tabs/home']);
+            console.log("Você saiu!!");
+        }
+    );
+}
+
+}
