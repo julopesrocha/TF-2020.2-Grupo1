9cba5ee93edfd691714a4140db36680650c9f7df
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-08-15 20:19:00 -0300

    + adiciona Seeder e Factory de Recipe

--- back/database/seeds/ChallengeTableSeeder.php
+++ back/database/seeds/ChallengeTableSeeder.php
@@ -11,6 +11,6 @@ class ChallengeTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Challenge::class, 20)->create();
+        factory(App\Challenge::class, 10)->create();
     }
 }
