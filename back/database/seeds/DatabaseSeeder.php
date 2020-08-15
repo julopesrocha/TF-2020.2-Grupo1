841860b50c2c427512d768738ec67b9a4cf557cf
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-08-15 20:42:00 -0300

    + adiciona Seeder e Factory de Comment

--- back/database/seeds/DatabaseSeeder.php
+++ back/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(ChallengeTableSeeder::class);
         $this->call(RecipeTableSeeder::class);
+        $this->call(CommentTableSeeder::class);
     }
 }
