6941a484c5b83c6841d1f4daff4a76af942156e3
gabimattos <50138800+gabimattos@users.noreply.github.com>
2020-08-19 18:02:23 -0300

    + gera guard, + importa guard, + adiciona funcionalidade na guard

--- motivate-app/src/app/tabs/tabs-routing.module.ts
+++ motivate-app/src/app/tabs/tabs-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import {AuthGuard} from '../guards/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -17,11 +18,11 @@ const routes: Routes = [
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule), canActivate:[AuthGuard]
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule), canActivate:[AuthGuard]
       },
       {
         path: '',
