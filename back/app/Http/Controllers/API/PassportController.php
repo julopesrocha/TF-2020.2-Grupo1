219edc9e53154b5966e51130cef3cac2d79c4457
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-08-19 01:30:00 -0300

    + formatação e documentação na PassportController

--- back/app/Http/Controllers/API/PassportController.php
+++ back/app/Http/Controllers/API/PassportController.php
@@ -12,33 +12,60 @@ use App\Notifications\UserNotification;
 
 class PassportController extends Controller
 {
+    /**
+     * Registra um novo usuário no BD
+     * 
+     * @param UserResource      $request
+     * 
+     * @return JsonResponse
+     */
     public function register(UserRequest $request) {
         $newUser = new User;
         $newUser->createUser($request);
         $success['token']=$newUser->createToken('MyApp')->accessToken;
         $newUser->notify(new UserNotification($newUser));
-        return response()->json(['success'=> $success, 'user'=>$newUser], 200);
+        return response()->json(['success'=> $success,
+                                 'user'=>new UserResource($newUser)], 200);
     }
 
+    /**
+     * loga um usuário e retorna uma token
+     * 
+     * @return JsonResponse
+     */
     public function login() {
-        if (Auth::attempt(['email'=>request('email'), 'password'=>request('password')])) {
+        if (Auth::attempt(['email'=>request('email'),
+                           'password'=>request('password')])) {
             $user = Auth::user();
             $success['token']=$user->createToken('MyApp')->accessToken;
-            return response()->json(['success'=>$success, 'user'=>$user], 200);
+            return response()->json(['success'=>$success,
+                                     'user'=>new UserResource($user)], 200);
         }
         else {
             return response()->json(['error'=>'Unauthorized'], 401);
         }
     }
 
+    /**
+     * retorna as informações do usuário logado
+     * 
+     * @return JsonResponse
+     */
     public function getDetails() {
         $user = Auth::user();
         return response()->json([$user],200);
     }
 
+    /**
+     * invalida o token do usuário logado
+     * 
+     * @return JsonResponse
+     */
     public function logout() {
         $accessToken = Auth::user()->token();
-        DB::table('oauth_refresh_tokens')->where('access_token_id',$accessToken->id)->update(['revoked'=>true]);
+        DB::table('oauth_refresh_tokens')
+                ->where('access_token_id',$accessToken->id)
+                ->update(['revoked'=>true]);
         $accessToken->revoke();
         return response()->json(['User has logged out'], 200);
     }
